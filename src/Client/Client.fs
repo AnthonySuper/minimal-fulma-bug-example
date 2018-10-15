49135cfae06550285007bfbf6b703bd32075b1ca
Anthony Super <anthony@noided.media>
2018-10-15 13:07:41 -0600

    It's not pretty but it works

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -13,52 +13,57 @@ open Shared
 
 open Fulma
 
+type PageModel =
+    | HomeModel of Routes.Home.Model 
+
 // The model holds data that you want to keep track of while the application is running
 // in this case, we are keeping track of a counter
 // we mark it as optional, because initially it will not be available from the client
 // the initial value will be requested from server
 type Model 
-    = { Counter: Counter option
-        Route: Route }
+    = { Route: Route
+        PageModel: PageModel } 
 
 // The Msg type defines what events/actions can occur while the application is running
 // the state of the application changes *only* in reaction to these events
 type Msg =
-| Increment
-| Decrement
 | ChangeRoute of Route
-| InitialCountLoaded of Result<Counter, exn>
+| HomeMsg of Routes.Home.Msg 
 
 // defines the initial state and initial command (= side-effect) of the application
 let init () : Model * Cmd<Msg> =
-    let initialModel = { Counter = None; Route = Home }
-    let loadCountCmd =
-        Cmd.ofPromise
-            (fetchAs<Counter> "/api/init")
-            []
-            (Ok >> InitialCountLoaded)
-            (Error >> InitialCountLoaded)
-    initialModel, loadCountCmd
+    let home, homeCmd = Routes.Home.init ()
+    let initialModel = { Route = Home; PageModel = HomeModel home }
+    initialModel, Cmd.none
+
+let private routeData r =
+    match r with
+    | Home -> 
+        let (m, c) = Routes.Home.init ()
+        HomeModel m, Cmd.none
+    | _ -> 
+        let (m, c) = Routes.Home.init ()
+        HomeModel m, Cmd.none
+    
+let private changeRoute r model =
+    let (d, cmd) = routeData r 
+    {model with Route = r; PageModel = d}, cmd 
 
 // The update function computes the next state of the application based on the current state and the incoming events/messages
 // It can also run side-effects (encoded as commands) like calling the server via Http.
 // these commands in turn, can dispatch messages to which the update function will react.
 let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
-    match currentModel.Counter, msg with
-    | Some x, Increment ->
-        let nextModel = { currentModel with Counter = Some (x + 1) }
-        nextModel, Cmd.none
-    | Some x, Decrement ->
-        let nextModel = { currentModel with Counter = Some (x - 1) }
-        nextModel, Cmd.none
-    | _, (ChangeRoute r) ->
-        let nextModel = { currentModel with Route = r } 
-        nextModel, Cmd.none
-    | _, InitialCountLoaded (Ok initialCount)->
-        let nextModel = { currentModel with Counter = Some initialCount }
-        nextModel, Cmd.none
+    match (currentModel.PageModel, msg) with
+    | (_, ChangeRoute r) -> changeRoute r currentModel 
+    | (HomeModel hm, HomeMsg m) ->
+        let (d, c) = Routes.Home.update m hm
+        {currentModel with PageModel = (HomeModel d)}, Cmd.none
     | _ -> currentModel, Cmd.none
 
+let viewRoute model dispatch = 
+    match model.PageModel with
+    | HomeModel m -> Routes.Home.view (dispatch << ChangeRoute) m (dispatch << HomeMsg)
+    | _ -> div [] []
 
 let safeComponents =
     let components =
@@ -78,9 +83,6 @@ let safeComponents =
           str " powered by: "
           components ]
 
-let show = function
-| { Counter = Some x } -> string x
-| { Counter = None   } -> "Loading..."
 
 let button txt onClick =
     Button.button
@@ -93,11 +95,8 @@ let button txt onClick =
 let view (model : Model) (dispatch : Msg -> unit) =
     div [ ]
         [ navDisp (dispatch << ChangeRoute) model.Route;
-          Summit.site;
+          viewRoute model dispatch
           div [] [ showRoute model.Route |> str ]
-          div [] [ show model |> str ]  
-          button "+" (fun _ -> dispatch Increment);
-          button "-" (fun _ -> dispatch Decrement)
         //   Container.container []
         //       [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
         //             [ Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ] ]
--- src/Client/Routes/Home.fs
+++ src/Client/Routes/Home.fs
@@ -1,3 +1,66 @@
 module Routes.Home
 
+open Route
 open Elmish
+open Fable.Helpers.React
+open Fable.Helpers.React.Props
+open Fulma
+
+    type Model
+        = { Count : int }
+
+    let init() =
+        { Count = 0 }, Cmd.none
+
+    type Msg =
+        | ChangeRoute of Route
+        | Increment
+        | Decrement
+
+    let update msg model =
+        match msg with
+        | Increment -> { model with Count = model.Count + 1 }, Cmd.none
+        | Decrement -> { model with Count = model.Count - 1 }, Cmd.none
+        | _ -> model, Cmd.none
+
+    let title =
+        Container.container 
+            [ Container.IsFluid 
+              Container.Modifiers
+                [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ]
+            ]
+            [ Heading.h1 [] [ str "Summit Investment Technologies" ]
+              Heading.h2 [ Heading.IsSubtitle ]
+                [ str "The Grease on the Cogs of Capitalism" ] ]
+
+    let button txt onClick =
+        Button.button
+            [ Button.IsFullWidth
+              Button.Color IsPrimary
+              Button.OnClick onClick ]
+            [ str txt ]
+
+    let counterParts model dispatch =  
+        [ button "+" (fun _ -> dispatch Increment) 
+          Notification.notification [Notification.Color IsInfo] [str (string model.Count)]
+          button "-" (fun _ -> dispatch Decrement)
+        ]
+
+    let counter model dispatch =
+        let parts = counterParts model dispatch
+        Level.level
+            [] 
+            (parts |> List.map (Level.item [] << fun x -> [x]))
+
+    let view routeDisp (model : Model) (dispatch : Msg -> Unit) =
+        Hero.hero []
+            [
+                Hero.head [] []
+                Hero.body []
+                    [ title; counter model dispatch ]
+                Hero.foot [] [] 
+            ]
+
+
+
+       
